a668375a6d558f78177a914f33dd1779ae83ddfa
xaoccc <xaocccc@gmail.com>
2025-06-20 15:16:24 +0300

    created a signal to create a user profile upon registration

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,4 +3,4 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_picture_url = models.CharField(max_length=300)
+    profile_picture_url = models.CharField(max_length=300, blank=True)
