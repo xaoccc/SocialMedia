b7128b9cc675be924e1999927525d3578e181e4c
xaoccc <xaocccc@gmail.com>
2025-08-14 16:11:17 +0300

    unittests init

--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+import unittest
+from accounts.models import Profile
 
-# Create your tests here.
+class ProfileUnitTest(unittest.TestCase):
+    def test_valid_profile_picture_url(self):
+        profile = Profile(profile_picture_url='http://www.www.www')
+        self.assertEqual(profile.profile_picture_url, 'http://www.www.www')
+    def test_invalid_profile_picture_url(self):
+        profile = Profile(profile_picture_url='abv')
+        self.assertEqual(profile.profile_picture_url, 'abv')
+
